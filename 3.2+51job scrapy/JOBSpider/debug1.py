7f939d9aef0bb25cb4d4479471d69c2c37adf926
axing <15736959834@163.com>
2018-11-11 14:37:17 +0800

    shanchu

--- 3.2+51job scrapy/JOBSpider/debug1.py
+++
@@ -1,5 +0,0 @@
-#conding:utf-8
-from scrapy import cmdline
-# cmdline.execute(['scrapy', 'crawl', '51job', '-o', 'job.csv', '-s', 'FEED_EXPORT_ENCODING=gb18030'])
-# cmdline.execute(['scrapy crawl zl'.split('')]  )
-cmdline.execute('scrapy crawl zl'.split(' '))
